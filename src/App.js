8f5da9d0c579776fb41730aab76ad88003c04c76
IreneOrtizTena <irene27ortiz@hotmail.com>
2023-01-27 11:09:45 +0100

    updating api calls

--- src/App.js
+++ src/App.js
@@ -16,17 +16,15 @@ import Uorder from "./Uorder";
 import axios from "axios";
 import Contact from "./Contact";
 
-
 export const menuContext = createContext();
 
 function App() {
-
-  const [dishes, setDishes] = useState()
-  const [categories, setCategories] = useState()
-  const [orders, setOrders] = useState()
-  const [spanish, setSpanish] = useState()
-  const [order, setOrder] = useState([])
-  const [total, settotal] = useState(0)
+  const [dishes, setDishes] = useState();
+  const [categories, setCategories] = useState();
+  const [orders, setOrders] = useState();
+  const [spanish, setSpanish] = useState();
+  const [order, setOrder] = useState([]);
+  const [total, settotal] = useState(0);
 
   function addItem(e) {
     e.preventDefault();
@@ -37,50 +35,61 @@ function App() {
     setOrder((i) => [...i, orderItem]);
   }
 
-
-  
-  
   // const [newOrder, setNewOrder] = useState([])
-  async function fetching  () {
-    axios('http://localhost:3000/dishes').then(i => setDishes(i.data))
-    axios('http://localhost:3000/categories').then(i => setCategories(i.data))
-     axios('http://localhost:3000/orders').then(i => setOrders(i.data))
-     
+  async function fetching() {
+    axios("https://restaurant-order-api.onrender.com/dishes").then((i) =>
+      setDishes(i.data)
+    );
+
+    axios("https://restaurant-order-api.onrender.com/categories").then((i) =>
+      setCategories(i.data)
+    );
+    axios("https://restaurant-order-api.onrender.com/orders").then((i) =>
+      setOrders(i.data)
+    );
   }
 
   useEffect(() => {
-
-    fetching()
-
-  },[])
-
+    fetching();
+  }, []);
 
   return (
     <>
-    
       <BrowserRouter>
-         <menuContext.Provider value={{total, settotal, addItem, order, setOrder, spanish, orders, setOrders, categories, setCategories, dishes, setDishes }}> 
-       
+        <menuContext.Provider
+          value={{
+            total,
+            settotal,
+            addItem,
+            order,
+            setOrder,
+            spanish,
+            orders,
+            setOrders,
+            categories,
+            setCategories,
+            dishes,
+            setDishes,
+          }}
+        >
           <div
             className="flex justify-between flex-col min-h-screen items-center bg-accent font-bold"
             data-theme="autumn"
           >
-            <Navbar /> 
+            <Navbar />
             <Routes>
               <Route path="/" element={<Hero />} />
               <Route path="/ordernow" element={<Ordernow />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/menu" element={<Menu />} /> 
+              <Route path="/menu" element={<Menu />} />
               <Route path="/spanishdishes" element={<Spanishdishes />} />
-              <Route path="/italiandishes" element={<Italiandishes />} /> 
-              <Route path="/booknow" element={<Booknow />} /> 
+              <Route path="/italiandishes" element={<Italiandishes />} />
+              <Route path="/booknow" element={<Booknow />} />
               <Route path="/uorder" element={<Uorder />} />
-              
             </Routes>
 
             <Footer />
-           
           </div>
         </menuContext.Provider>
       </BrowserRouter>
